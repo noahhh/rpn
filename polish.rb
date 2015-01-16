823fe4f78aca7cb3ca840792aaac5e5eb988174e
Noah Holmes <noah.r.holmes@gmail.com>
2015-01-15 20:41:07 -0500

    small changes

--- polish.rb
+++ polish.rb
@@ -1,27 +1,34 @@
-
 class RPN
-
     def evaluate
         evaluation = []
         until evaluation.include?('q')
-        expression = gets.split 				# splits input on breaks
-        operands = []
-        expression.each do |x|
-            case x
-                when /\d/ 							# When a digit, do this
-                    evaluation.push(x.to_f)			# turns evaluation to float
-                when "-", "/", "*", "+", "**"			# When it hits an operand, do this
-                    operands = evaluation.pop(2)		# pop last two numbers off, assign them to operands
-                    evaluation.push(operands[0].send(x, operands[1]))  # push new evaluation in to array
+        input = gets.split 				                             # splits input on breaks
+        symbols = []
+        input.each do |a|
+            case a
+                when "-", "/", "*", "+"			                     # When it hits an operand, do this
+                if evaluation.size > 1
+                    symbols = evaluation.pop(2)		             # pop last two numbers off, assign them to operands
+                    evaluation.push(symbols[0].send(a, symbols[1]))  # push new evaluation in to array
+                else
+                    evaluation << input
+                end
+                when /\d/                                            # When a digit, do this
+                    evaluation.push(a.to_f)                          # turns evaluation to float
                 when "q"
                     puts "Quitting"
                     exit
+                end
             end
+            puts evaluation
         end
-        puts evaluation
     end
-end
 end
 
 calc = RPN.new
 calc.evaluate
+
+
+
+# Need to bank the symbols if put in before two numbers
+# breaks when more symbols than numbers are in there
