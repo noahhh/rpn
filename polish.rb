65b423dc086ecea771a24bacd25b619e91e57be7
Noah Holmes <noah.r.holmes@gmail.com>
2015-01-16 12:42:43 -0500

    fixing some bugs

--- polish.rb
+++ polish.rb
@@ -8,13 +8,13 @@ class RPN
             case a
                 when "-", "/", "*", "+"			                     # When it hits an operand, do this
                 if evaluation.size > 1
-                    symbols = evaluation.pop(2)		             # pop last two numbers off, assign them to operands
-                    evaluation.push(symbols[0].send(a, symbols[1]))  # push new evaluation in to array
+                    symbols = evaluation.pop(2)		             # pop last two numbers off (operand is in last place), assign them to symbols
+                    evaluation.push(symbols[0].send(a, symbols[1]))  # push
                 else
-                    evaluation << input
+                    puts "you can't do that."
                 end
                 when /\d/                                            # When a digit, do this
-                    evaluation.push(a.to_f)                          # turns evaluation to float
+                    evaluation.push(a.to_f)                          # pushes input as float to evaluation array
                 when "q"
                     puts "Quitting"
                     exit
@@ -29,6 +29,7 @@ calc = RPN.new
 calc.evaluate
 
 
+# DONE?
+# Need to bank the symbols if put in before two numbers/tell them they can't do that
 
-# Need to bank the symbols if put in before two numbers
 # breaks when more symbols than numbers are in there
